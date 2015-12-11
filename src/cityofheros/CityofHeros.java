0be52d608ca1137bac7adf4ac1bc3d6774e83f1d
Daniel <Daniel@192.168.1.144>
2015-12-10 17:40:05 -0700

    test commit

--- src/cityofheros/CityofHeros.java
+++ src/cityofheros/CityofHeros.java
@@ -12,7 +12,6 @@ import model.Player;
 import View.MainMenu;
 import View.showHelp;
 
-
 /**
  *
  * @author Josch
@@ -23,58 +22,35 @@ public class CityofHeros {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Player player = new Player();
-        
-     WelcomeView welcomeView = new WelcomeView();
+
+        WelcomeView welcomeView = new WelcomeView();
         welcomeView.displayBanner();
-        
-    player.setName(welcomeView.getPlayerName());
-      
-     welcomeView.displayPlayerNameBanner(player);
-        
+
+        player.setName(welcomeView.getPlayerName());
+
+        welcomeView.displayPlayerNameBanner(player);
+
         MainMenu mainView = new MainMenu();
         mainView.display();
-        
+
      // char in = mainView.getInput();
-       
-      // System.out.println("YOU ENTERED " + in);
-       
-      // showHelp Help = new showHelp();
-       
-      //* if(in == 'H'){
-           
-       
-   //      char opt = Help.opt();
-       
-     //  System.out.println("YOU ENTERED " + opt);
-       
-       //if(opt == 'G'|| opt == 'C' || opt == 'B'){
-          
+        // System.out.println("YOU ENTERED " + in);
+        // showHelp Help = new showHelp();
+        //* if(in == 'H'){
+        //      char opt = Help.opt();
+        //  System.out.println("YOU ENTERED " + opt);
+        //if(opt == 'G'|| opt == 'C' || opt == 'B'){
         //Help.opt();
-       //}
-       
+        //}
         //Exit goodbye = new Exit();
-      
-          // if(in == 'E'){
-            //   char exit = goodbye.nomore();
-              // System.out.println("You Entered " + exit);
-        
+        // if(in == 'E'){
+        //   char exit = goodbye.nomore();
+        // System.out.println("You Entered " + exit);
         //  Map map = new Map();
-   //  map.initialize();
-        
-     //System.out.println(map.toString());
-               
-           
-       }
-               
-           }
-   
-      
-           
-       
-      
-    
-    
+        //  map.initialize();
+        //System.out.println(map.toString());
+    }
 
-
+}
